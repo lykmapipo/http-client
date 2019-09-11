fa53b4ee2a922137ca98d37b176497e0cf088e1d
lykmapipo <lallyelias87@gmail.com>
2019-09-11 12:25:02 +0300

    feat(utils): add map response to error

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { mergeObjects } from '@lykmapipo/common';
+import { mergeObjects, assign } from '@lykmapipo/common';
 import { getString } from '@lykmapipo/env';
 
 /**
@@ -29,7 +29,7 @@ export const CONTENT_TYPE = 'application/json';
  * @example
  *
  * const optns = { baseUrl: ... };
- * const options = withDefaults(optns)
+ * const options = withDefaults(optns);
  * // => {baseUrl: ..., headers: { ... } };
  */
 export const withDefaults = optns => {
@@ -50,7 +50,7 @@ export const withDefaults = optns => {
  * @function mapResponseToData
  * @name mapResponseToData
  * @description Convert raw http response to data
- * @param {object} response axios http response
+ * @param {object} rawResponse raw http response
  * @returns {object} response data
  * @author lally elias <lallyelias87@mail.com>
  * @license MIT
@@ -60,7 +60,68 @@ export const withDefaults = optns => {
  * @public
  * @example
  *
- * const data = mapResponseToData(httpResponse).
+ * const data = mapResponseToData(rawResponse).
  * // => { .. };
  */
-export const mapResponseToData = response => response.data;
+export const mapResponseToData = rawResponse => rawResponse.data;
+
+/**
+ * @function mapResponseToError
+ * @name mapResponseToError
+ * @description Convert raw http response error to js native error
+ * @param {object} rawResponse raw http response
+ * @returns {Error} response error
+ * @see {@link https://github.com/axios/axios#handling-errors}
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * const error = mapResponseToError(rawResponse).
+ * // => Error;
+ */
+export const mapResponseToError = rawResponse => {
+  // obtain error details
+  let { code, status, message, description, stack, errors, data } = rawResponse;
+  const { request, response } = rawResponse;
+
+  // handle server response error
+  if (response) {
+    code = response.code || code;
+    status = response.status || status;
+    data = response.data || data || {};
+    message = data.message || response.statusText || message;
+    description = description || message;
+    errors = response.errors || errors || {};
+    stack = response.stack || data.stack || stack;
+  }
+
+  // handle server not responding error
+  else if (request) {
+    code = code || 503;
+    status = status || 503;
+    description = description || 'Service Unavailable';
+    message = message || description;
+  }
+
+  // handle request setup error
+  else {
+    code = code || 400;
+    status = status || 400;
+    description = description || 'Bad Request';
+    message = message || description;
+  }
+
+  // initialize error
+  const error = new Error(message);
+  error.stack = stack;
+
+  // update error object
+  assign(error, { code, status, message, description, errors, ...data });
+
+  // return normalized native error
+  return error;
+};
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,5 +1,9 @@
 import { expect, faker } from '@lykmapipo/test-helpers';
-import { withDefaults, mapResponseToData } from '../src/utils';
+import {
+  withDefaults,
+  mapResponseToData,
+  mapResponseToError,
+} from '../src/utils';
 
 describe('client utils', () => {
   it('should prepare default options', () => {
@@ -34,5 +38,46 @@ describe('client utils', () => {
     const response = { data: [] };
     const data = mapResponseToData(response);
     expect(data).to.exist.and.be.eql(response.data);
+  });
+
+  it('should map response to error on request setup', () => {
+    let rawResponse = new Error('Request Failed');
+    let error = mapResponseToError(rawResponse);
+    expect(error).to.exist.and.be.an.instanceof(Error);
+    expect(error.status).to.be.equal(400);
+    expect(error.code).to.be.equal(400);
+    expect(error.message).to.be.equal(rawResponse.message);
+    expect(error.description).to.be.equal('Bad Request');
+
+    rawResponse = { message: 'Request Failed' };
+    error = mapResponseToError(rawResponse);
+    expect(error).to.exist.and.be.an.instanceof(Error);
+    expect(error.status).to.be.equal(400);
+    expect(error.code).to.be.equal(400);
+    expect(error.message).to.be.equal(rawResponse.message);
+    expect(error.description).to.be.equal('Bad Request');
+  });
+
+  it('should map response to error on server no response', () => {
+    const rawResponse = { message: 'Request Failed', request: {} };
+    const error = mapResponseToError(rawResponse);
+    expect(error).to.exist.and.be.an.instanceof(Error);
+    expect(error.status).to.be.equal(503);
+    expect(error.code).to.be.equal(503);
+    expect(error.message).to.be.equal(rawResponse.message);
+    expect(error.description).to.be.equal('Service Unavailable');
+  });
+
+  it('should map response to error on server response', () => {
+    const rawResponse = {
+      message: 'Cast Error',
+      response: { code: 400, status: 400 },
+    };
+    const error = mapResponseToError(rawResponse);
+    expect(error).to.exist.and.be.an.instanceof(Error);
+    expect(error.status).to.be.equal(rawResponse.response.code);
+    expect(error.code).to.be.equal(rawResponse.response.status);
+    expect(error.message).to.be.equal(rawResponse.message);
+    expect(error.description).to.be.equal(rawResponse.message);
   });
 });
